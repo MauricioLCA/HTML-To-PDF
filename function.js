d8537458e036ca6d184b1718b9d680c232b5c74c
MauricioLCA <157726545+MauricioLCA@users.noreply.github.com>
2024-03-24 15:23:00 -0600

    function.js

--- function.js
+++ function.js
@@ -150,10 +150,10 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
 	  <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.min.js"></script>
 	  <style>${customCSS}</style>
 	  <div class="main">
+	  <div id="content">${html}</div>
 	  <div class="header">
 		<button class="button" id="download">Download</button>
-	  </div>
-	  <div id="content">${html}</div>
+	  </div>   
 	  </div>
 	  <script>
 	  document.getElementById('download').addEventListener('click', function() {
