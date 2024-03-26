5c4623a7ef3f291be5dc8d54acd14b6d4b77af2a
MauricioLCA <157726545+MauricioLCA@users.noreply.github.com>
2024-03-26 12:33:33 -0600

    function.js

--- function.js
+++ function.js
@@ -89,34 +89,26 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
 	}
   
 	button#download {
-	  position: fixed;
+ 	  position: fixed;
+	  top: 8px;
+	  left: 50%;
+	  transform: translateX(-50%);
 	  border-radius: 0.5rem;
+	  padding: 8px 16px;
+	  background-color: #23c1da;
+	  color: #fff;
 	  font-size: 14px;
 	  font-weight: 600;
 	  line-height: 1.5rem;
-	  color: #0d0d0d;
 	  border: none;
-	  font-family: 'Inter';
-	  padding: 0px 12px;
-	  height: 32px;
-	  background: #ffffff;
-	  top: 8px;
-	  right: 8px;
-	  box-shadow: 0 0 0 0.5px rgba(0, 0, 0, 0.08), 0 1px 2.5px rgba(0, 0, 0, 0.1);
+	  font-family: 'Open Sans';
+	  height: 40px;
+	  width: 50%;
 	  cursor: pointer;
-	}
-  
-	button#download:hover {
-	  background: #f5f5f5;
-	  box-shadow: 0 0 0 0.5px rgba(0, 0, 0, 0.12), 0 2px 4px rgba(0, 0, 0, 0.06), 0 6px 12px -3px rgba(0, 0, 0, 0.1);
-	}
-  
-	button#download.downloading {
-	  color: #ea580c;
-	}
-  
-	button#download.done {
-	  color: #16a34a;
+ 	}
+
+  	button#download:hover {
+	  background-color: #19a5bb;
 	}
   
 	::-webkit-scrollbar {
@@ -136,7 +128,7 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
 	  <style>${customCSS}</style>
 	  <div class="main">
 	  <div class="header">
-		<button class="button" id="download">Download</button>
+		<button class="button" id="download">Descargar</button>
 	  </div>
 	  <div id="content">${html}</div>
 	  </div>
@@ -144,7 +136,7 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
 	  document.getElementById('download').addEventListener('click', function() {
 		var element = document.getElementById('content');
 		var button = this;
-		button.innerText = 'Downloading...';
+		button.innerText = 'Descargando...';
 		button.className = 'downloading';
   
 		var opt = {
@@ -163,10 +155,10 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
 		}
 		};
 		html2pdf().set(opt).from(element).toPdf().get('pdf').then(function(pdf) {
-		button.innerText = 'Done 🎉';
-		button.className = 'done';
+		button.innerText = 'Descargado';
+		button.className = 'Done';
 		setTimeout(function() { 
-		  button.innerText = 'Download';
+		  button.innerText = 'Descargar';
 		  button.className = ''; 
 		}, 2000);
 		}).save();
